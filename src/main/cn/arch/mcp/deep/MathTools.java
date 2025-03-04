f5999635dc6c1ddc12d2ec5f76f3258893c52188
ag.wan <ag.wan@huolala.cn>
2025-03-04 15:31:51 +0800

    init

---
+++ src/main/cn/arch/mcp/deep/MathTools.java
@@ -0,0 +1,9 @@
+package cn.arch.mcp.deep;
+
+public class MathTools {
+
+    public static int add(int a, int b) {
+        return a;
+    }
+
+}
---
+++ src/main/cn/arch/mcp/deep/McpDeepApplication.java
@@ -0,0 +1,13 @@
+package cn.arch.mcp.deep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpDeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpDeepApplication.class, args);
+    }
+
+}
